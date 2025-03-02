23af2ae450a97bb474de2a3ca1922b86234d25de
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2025-03-02 09:15:19 +0530

    tailwind fixed

---
+++ FrontEnd/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Adjust according to your project's file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
